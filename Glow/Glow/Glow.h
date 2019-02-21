4ad94ccd1166b576d75bdd5c1d5d136cb06bad6f
marcprux <marc@prux.org>
2019-02-20 22:13:00 -0500

    Checkpoint

---
+++ Glow/Glow/Glow.h
@@ -0,0 +1,19 @@
+//
+//  Glow.h
+//  Glow
+//
+//  Created by Marc Prud'hommeaux on 10/9/18.
+//  Copyright © 2018 Glimpse I/O. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Glow.
+FOUNDATION_EXPORT double GlowVersionNumber;
+
+//! Project version string for Glow.
+FOUNDATION_EXPORT const unsigned char GlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Glow/PublicHeader.h>
+
+
---
+++ Glow/Glow/GlowModel.swift
@@ -0,0 +1,9 @@
+//
+//  GlowModel.swift
+//  Glow
+//
+//  Created by Marc Prud'hommeaux on 10/11/18.
+//  Copyright © 2018 Glimpse I/O. All rights reserved.
+//
+
+import Foundation
